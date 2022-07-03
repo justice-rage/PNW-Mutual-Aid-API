5ad94e6adeb34b9777124177c280aa8957e11d17
justicepelteir <justice.douglas@hey.com>
2022-07-03 15:32:52 -0700

    WIP: search method

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -3,4 +3,12 @@ class Resource < ApplicationRecord
   validates :category, presence: true
   validates :website, presence: true
   validates :address, presence: true
+
+  def self.search(search)
+    if search
+      where(["name ILIKE ?","%#{search}%"])
+    else
+      all
+    end
+  end
 end
