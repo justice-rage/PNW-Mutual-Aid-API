bb66c8b480fd57dd9f9fd92809bb8b16ae61eddb
justicepelteir <justice.douglas@hey.com>
2022-07-03 15:34:50 -0700

    WIP: action method blueprint

---
+++ app/controllers/resources_controller.rb
@@ -0,0 +1,42 @@
+class ResourcesController < ApplicationController
+
+  def index
+    # @resources = {"quotation": "This api is calling."} => working
+    @resources = Resources.all
+    json_response(@resources)
+  end
+
+  def show
+    @resource = Resources.find(params[:id])
+    json_response(@resource)
+  end
+
+  def create
+    @resource = Resources.create!(resource_params)
+    json_response(@resource. :created)
+  end
+
+  def update
+    @resource = Resource.find(params[:id])
+    if @resource.update!(resource_params)
+      render status: 200, json: {
+        message: "Updated Successfully!"
+      }
+    end
+  end
+
+  def destroy
+    @resource = Resource.find(params[:id])
+    if @resource.destroy
+      render status: 200, json: {
+        message: "Deleted Successfully"
+      }
+    end
+  end
+
+  private
+  def resource_params
+    params.permit(:name, :website, :category, :address)
+  end
+
+end
