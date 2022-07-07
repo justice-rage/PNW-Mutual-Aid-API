e3913c5290248186107d637587425c364b8ad6e6
justicepelteir <justice.douglas@hey.com>
2022-07-06 19:55:26 -0700

    returns RecordInvalid error - passes test

--- spec/requests/exceptions_spec.rb
+++ spec/requests/exceptions_spec.rb
@@ -10,3 +10,14 @@ describe 'get a resource route with exception handler', :type => :request do
 
 end
 
+describe 'post a resource route with exception handlers', :type => :request do
+
+  before do
+    post '/resources', params: { :name => 'goodwill but actually good', :website => "betterwill.com", :category => "housing", :address => nil }
+  end
+
+  it 'returns a RecordInvalid error' do
+    expect(response).to have_http_status(:unprocessable_entity)
+  end
+end
+
