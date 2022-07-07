be0b3fe6313604370d57e16690a213ae84bf2354
justicepelteir <justice.douglas@hey.com>
2022-07-06 20:01:50 -0700

    update seed layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_resources
+  end
+  
+  def generate_resources
+    100.times do |i|
+
+      category_type = ['food', 'housing', 'education', 'lgbtq', 'animal', 'health'].sample
+
+      resource = Resource.create!(
+          name: (Faker::App.name),
+          website: (Faker::Internet.domain_name), 
+          category: (category_type),
+          address: (Faker::Address.city)
+      )
+
+      puts "Resource: #{i},
+      name: #{resource.name}, 
+      website: #{resource.website}, 
+      category: #{resource.category}, 
+      address: #{resource.address}"
+    end
+  end
+end
+  
+Seed.begin
