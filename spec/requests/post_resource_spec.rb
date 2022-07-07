d95b13fa6ac5480a7f37b954236caba8e113009d
justicepelteir <justice.douglas@hey.com>
2022-07-06 19:59:21 -0700

    DRY up testing

--- spec/requests/post_resource_spec.rb
+++ spec/requests/post_resource_spec.rb
@@ -5,19 +5,10 @@ describe 'post a resource route', :type => :request do
     post '/resources', params: { :name => 'NW Bike collective', :website => 'www.nwbc.org', :category => 'vehicles and maintanence', :address => '123 bumblebee' }
   end
 
-  it 'returns the resource name' do 
+  it 'returns a created resource' do 
     expect(JSON.parse(response.body)['name']).to eq('NW Bike collective')
-  end
-
-  it 'returns the resource website' do
     expect(JSON.parse(response.body)['website']).to eq('www.nwbc.org')
-  end
-
-  it 'returns the resource category' do
     expect(JSON.parse(response.body)['category']).to eq('vehicles and maintanence')
-  end
-
-  it 'returns the resource address' do
     expect(JSON.parse(response.body)['address']).to eq('123 bumblebee')
   end
 
